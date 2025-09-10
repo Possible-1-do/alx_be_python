9267357917883f575869bd2065ada60f0c430123
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-09-10 23:45:43 +0000

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#Define Numbers
+number1 = 10
+number2 = 5
+
+#Performing operations
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#Printing result
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
