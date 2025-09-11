8582c1d701352da1f3047eae6c63eaea32da9da9
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-09-11 04:43:34 +0000

    Add hours_to_seconds.py

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+#Converting hours to seconds
+
+#Variables
+hours = 2
+number_of_seconds = 3600
+seconds = hours * number_of_seconds
+
+#Printing result
+print(hours, "hour(s) is" ,seconds, "seconds")
