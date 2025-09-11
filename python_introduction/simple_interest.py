655cb05671180c6ee80c007545086ec9e58be29d
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-09-11 04:18:07 +0000

    Add simple_interest.py into python_intoroduction folder

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Simple interest
+#Variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#Formula for calculating simple interest
+interest = principal * rate * time
+
+#Printing the result
+print("The simple interest is:", interest)
