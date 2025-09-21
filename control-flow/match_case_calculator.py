416cd3a003c1326444a64e5d64970edf9d3b6c7e
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-09-21 15:06:55 +0000

    Added match case calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Chose an operation!(+,-,*,/)")
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = int(input("Enter your choice:"))
+
+match choice:
+    case 1:
+        print("The result is",num1 + num2)
+    case 2:
+        print("The result is",num1 - num2)
+    case 3:
+        print("The result is",num1 * num2)
+    case 4:
+        if num2 != 0:
+            print("The result is",num1 / num2)
+        else:
+            print("Cannot divide by zero")
+    case _:
+        print("You entered a wrong choice")
+         
