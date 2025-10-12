74c1072a278b5a83a84982cad4d3137e5b8a5162
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-10-12 22:40:18 +0000

    Bank account codes added.

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        # This attribute is encapsulated 
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        """Add money to the account"""
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        """Withdraw money if funds are enough"""
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        """Show the current account balance"""
+        print(f"Current Balance: ${self.account_balance}")
