fa9d1dca0138d26974c0f5183ce70d662c45b752
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-10-12 21:59:06 +0000

    Corrected version of library system.

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,41 +5,37 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
-#  EBook
+# EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        # Call the constructor of the base class
         super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} [EBook, {self.file_size}MB]"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 # PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        # Call the constructor of the base class
         super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} [PrintBook, {self.page_count} pages]"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
-# Library Structure
+# Library structure
 class Library:
     def __init__(self):
-        self.books = []  # list to store all book objects
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
-        print(f"Added: {book}")
 
     def list_books(self):
-        print("\nLibrary Collection:")
         for book in self.books:
-            print("-", book)
+            print(book)
